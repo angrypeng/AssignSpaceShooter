99200c83e571f9222ca714822eddfff733295269
kym2-09-PC\이우형 <eookog@gmail.com>
2017-01-06 17:13:31 +0900

    미사일바꿈 미사일 유도탄 AI로 적 우주선에 접근 완료

--- Assets/02.Scripts/Missile.cs
+++ Assets/02.Scripts/Missile.cs
@@ -5,6 +5,26 @@ using UnityEngine;
 public class Missile : MonoBehaviour {
 
 	Transform target;
+	public float speed = 0.1f;
+
+	public void GetPos(Transform trans){
+		target = trans;
+	}
+
+	void MissileControl(){
+		Vector3 dir = target.position - transform.position;
+		dir.Normalize ();
+
+		Quaternion targetQ = Quaternion.LookRotation (dir);
+
+		transform.rotation = Quaternion.Lerp (transform.rotation,
+			targetQ,
+			1.0f * Time.deltaTime);
+		Vector3 pos = Vector3.forward * Time.deltaTime * 2.0f;
+		transform.Translate (pos);
+	}
+
+	void On
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +33,6 @@ public class Missile : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		MissileControl ();
 	}
 }
--- Assets/02.Scripts/PlayerMove.cs
+++ Assets/02.Scripts/PlayerMove.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
 
+	public GameObject missile;
+	public Transform firePos;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,21 @@ public class PlayerMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetButtonUp ("Fire1")) {
+			GameObject fire = Instantiate (missile) as GameObject;
+			fire.transform.position = firePos.position;
+
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+			RaycastHit hit;
+
+			if(Physics.Raycast(ray, out hit)){
+				if(hit.transform.tag.Equals("Enemy")){
+					fire.GetComponent<Missile> ().GetPos(hit.transform);
+					Debug.Log (hit.transform.position);
+				}
+			}
+
+		}
 	}
 }
